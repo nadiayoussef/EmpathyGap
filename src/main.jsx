d800bace8d91217edae3e105c42cdf7c836d19a3
nadiayoussef <nadia.youssef@gmail.com>
2025-12-05 17:46:43 -0500

    fixed screen size

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ const App = () => {
       </nav> */}
 
       {view === "quiz" && <EmpathyQuiz />}
-      {view === "results" && <QuizResultsChart width={4356} height={2110} />}
+      {view === "results" && <QuizResultsChart width={4256} height={2110} />}
     </div>
   );
 };
