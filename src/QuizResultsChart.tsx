65864afa1a2f820ef92c0833eb4ccd92cead5818
nadiayoussef <nadia.youssef@gmail.com>
2025-12-12 15:03:27 -0500

    Screen size

--- src/QuizResultsChart.tsx
+++ src/QuizResultsChart.tsx
@@ -316,7 +316,7 @@ export const QuizResultsChart = ({ width, height }: QuizResultsChartProps) => {
   return (
     <div>
       <svg width="100%" height={height}
-      viewBox='0 0 ${width} ${height}'
+      viewBox={`0 0 ${width} ${height}`}
       preserveAspectRatio='none'>
         <rect width={width} height={height} fill="#fffaf0" />
         <g transform={`translate(${MARGIN.left},${MARGIN.top})`}>
--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ const App = () => {
       </nav> */}
 
       {view === "quiz" && <EmpathyQuiz />}
-      {view === "results" && <QuizResultsChart width={4156} height={1910} />}
+      {view === "results" && <QuizResultsChart width={4156} height={2010} />}
     </div>
   );
 };
